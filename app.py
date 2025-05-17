0347fd2ed07590a816cf6bb4f9a26ee116486d81
Leila Obalan <leilaobalan03@gmail.com>
2025-05-17 13:45:58 +0800

    feat: /api/chat intergrated

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return 'Server is running'
+    return 'Flask backend is running.'
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    user_input = data.get('message', '').strip().lower()
+
+    if user_input == 'hi':
+        response = 'hello'
+    else:
+        response = "i don't understand"
+
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
